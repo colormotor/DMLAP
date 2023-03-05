484599cb3a7385f37ceffbb088c76ba61b7e6d75
colormotor <drand48@gmail.com>
2023-03-05 18:10:55 +0000

    100th epoch and graphics test

--- python/py5sketches/pix2pix_graphics.py
+++ python/py5sketches/pix2pix_graphics.py
@@ -7,8 +7,7 @@ from tensorflow import keras
 import cv2
 
 # Create video input
-w, h = 512, 512
-vid = canvas.VideoInput(size=(w, h))
+w, h = 512, 256
 
 model_path = '../models/edges2comics/e100_generator.hd5'
 # Load pix2pix model
@@ -21,13 +20,6 @@ def generate_image(model, img):
     res = model(np.expand_dims(img, 0), training=True)[0].numpy()
     return (res*0.5 + 0.5) # Scale to [0, 1] (Canvas is fine with that)
 
-# Applies canny edge detection to our input
-def apply_canny_skimage(img, sigma=0.5):
-    import cv2
-    from skimage import feature
-    grayimg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    edges = (feature.canny(grayimg, sigma=sigma)*255).astype(np.uint8)
-    return cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 
 def setup():
     sketch.create_canvas(w, h)
@@ -36,11 +28,23 @@ def setup():
 
 def draw():
     c = sketch.canvas
-
+    # We draw on black background and white strokes
     c.background(0)
+    c.stroke(255)
+    c.no_fill()
+    c.stroke_weight(1.0)
+    # Geneate a simple animation
+    np.random.seed(120)
+    c.push()
+    c.translate(c.height/2, c.height/2) # Translate to center of left part of canvas
+    c.rotate(sketch.frame_count*0.1)    # and rotate continuosly
+    # draw some random circles
+    for i in range(20):
+        c.circle(np.random.uniform(-c.height/2, c.height/2, 2), np.random.uniform(5, 30))
+    c.pop()
 
-    # Read video and compute landmarks
-    frame = vid.read()
-    edges = apply_canny_skimage(frame)
-    res = generate_image(generator, cv2.resize(edges, (256, 256)))
-    c.image(res, [0,0], [c.width, c.height])
+    # Get half of canvas
+    img = c.get_image()[:,:256,:]
+    # Generate and draw result
+    result = generate_image(generator, img)
+    c.image(result, [256,0], [256, 256])
